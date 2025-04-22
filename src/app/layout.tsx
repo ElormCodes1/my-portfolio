b53fd46a3a86a439a4bf562816c5d235397d2cf3
ElormCodes1 <marriondokosi@gmail.com>
2025-04-22 08:56:42 +0000

    tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Footer from "@/components/Footer/page";
 import Header from "@/components/Header/page";
 import ScrollToTop from "@/components/ScrollToTop/page";
 import { Providers } from "./providers";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head />
+      {/* <head /> */}
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "r81ncos7ta");`,
+          }}
+        />
+        {/* <meta name="google-site-verification" content="aEVhP6SO-Y6kLUeYOcCggJkvEJOnE4ht86bz_IliWtM" /> */}
+      </head>
       <body className="dark:bg-black">
         <Providers>
           <Header />
@@ -29,6 +43,7 @@ export default function RootLayout({
           <ScrollToTop />
         </Providers>
       </body>
+      <GoogleAnalytics gaId="G-PP8C5JFLJD" />
     </html>
   );
 }
